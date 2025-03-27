cc7ee60a4cec3c974194c9503ca5a1ca2ae712fa
Abdulaziz <73533363+Abdulaziz168@users.noreply.github.com>
2025-03-27 15:47:51 +0500

    Update media_filter.py

--- modules/media_filter.py
+++ modules/media_filter.py
@@ -1,7 +1,16 @@
 async def handle_media(update, context):
     user = update.message.from_user
-    await update.message.delete()
-    await update.message.reply_text(
-        f"⚠️ {user.mention_html()} rasm, video yoki fayl yuborish taqiqlangan!",
-        parse_mode="HTML"
-    )
+    chat_id = update.message.chat_id
+    user_id = user.id
+
+    # Adminlar ro'yxatini guruhdan avtomatik olish
+    chat_administrators = await context.bot.get_chat_administrators(chat_id)
+    admin_ids = [admin.user.id for admin in chat_administrators]
+
+    # Faqat oddiy user bo‘lsa — o‘chir
+    if user_id not in admin_ids:
+        await update.message.delete()
+        await update.message.reply_text(
+            f"⚠️ {user.mention_html()} rasm, video yoki fayl yuborish taqiqlangan!",
+            parse_mode="HTML"
+        )
